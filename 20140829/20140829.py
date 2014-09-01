ef237b56cbc5456881a6895f09d7d6222bad99a6
oyxhm <376841842@qq.com>
2014-09-01 10:40:58 +0800

    add 20140829

---
+++ 20140829/20140829.py
@@ -0,0 +1,5 @@
+#alphabetical -----> phil
+a = 'alphabetical'
+for i in a[2:] :
+	if i == 'p' or i == 'h'or i == 'i' or i == 'l' :
+		print i,
---
+++ 20140829/readme.md
@@ -0,0 +1,3 @@
+**题目要求**
+
+输入“alphabetical”的时候，输出“pbil”
